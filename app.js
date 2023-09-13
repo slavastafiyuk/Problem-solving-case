e0a1e3edb2e7a78c6cc659e1fb8768d756b81e61
slavastafiyuk <73066939+slavastafiyuk@users.noreply.github.com>
2023-09-14 00:08:49 +0100

    Unit_test

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
 const fs = require('fs');
 const readline = require('readline');
 
-// Function to process the input CSV file
 function processInput(inputFileName) {
+  
+  if (!doesFileOrDirectoryExist(inputFileName)) {
+    return 0;
+  } 
+
   const productQuantities = new Map();
   const productBrands = new Map();
   const lineCount = countLines(inputFileName);
   let total_orders = 0
 
   console.log(1 < lineCount)
-  if(1 < lineCount == true && lineCount<Math.pow(10, 4) == true){
+  if (1 < lineCount == true && lineCount < Math.pow(10, 4) == true) {
     const rl = readline.createInterface({
       input: fs.createReadStream(inputFileName),
       output: process.stdout,
       terminal: false
     });
-  
+
     rl.on('line', (line) => {
       const [id, area, productName, quantity, brand] = line.split(',');
       const parsedQuantity = parseInt(quantity);
-      
-      // Update product quantities
       if (productQuantities.has(productName)) {
         productQuantities.set(productName, productQuantities.get(productName) + parsedQuantity);
       } else {
         productQuantities.set(productName, parsedQuantity);
       }
-      // Update product brands
       if (!productBrands.has(productName)) {
         productBrands.set(productName, new Map());
       }
@@ -35,17 +36,12 @@ function processInput(inputFileName) {
       } else {
         productBrands.get(productName).set(brand, 1);
       }
-  
       total_orders++
     });
-  
     rl.on('close', () => {
-      // Calculate average quantities
       const averageQuantities = Array.from(productQuantities.entries()).map(([product, quantity]) => {
         return [product, (quantity / total_orders).toFixed(3)];
       });
-  
-      // Calculate most popular brands
       const popularBrands = Array.from(productBrands.entries()).map(([product, brandsMap]) => {
         let maxBrand = '';
         let maxCount = 0;
@@ -57,32 +53,35 @@ function processInput(inputFileName) {
         }
         return [product, maxBrand];
       });
-  
-      // Create output files
       const outputFileName0 = `0_${inputFileName}`;
       const outputFileName1 = `1_${inputFileName}`;
-  
-      // Write output to files
       writeOutput(outputFileName0, averageQuantities);
       writeOutput(outputFileName1, popularBrands);
     });
-  }else{
-    if(1 < lineCount == true){
-      console.log('Minimum amount of lines is 2')
-    }else if(lineCount > Math.pow(10,4) == true){
-      console.log('Maximum amount of lines is 10^4')
+  } else {
+    if (1 >= lineCount == true) {
+      return 'Minimum amount of lines is 2'
+    } else if (lineCount > Math.pow(10, 4) == true) {
+      return 'Maximum amount of lines is 10^4'
     }
   }
-  
-}
-//Line counter
-function countLines(stream) {
-    const data = fs.readFileSync(inputFileName, 'utf8');
-  const lines = data.split('\n');
-  return lines.length;
-}
 
-// Function to write data to an output file
+}
+function countLines(file) {
+  if (!doesFileOrDirectoryExist(file)) {
+    return 0;
+  } else {
+    const data = fs.readFileSync(file, 'utf8');
+    if (!data.trim()) {
+      return 0;
+    }
+    const lines = data.split('\n');
+    return lines.length;
+  }
+}
+function doesFileOrDirectoryExist(path) {
+  return fs.existsSync(path);
+}
 function writeOutput(outputFileName, data) {
   const header = 'Product,Value\n';
   fs.writeFileSync(outputFileName, header);
@@ -90,13 +89,11 @@ function writeOutput(outputFileName, data) {
     fs.appendFileSync(outputFileName, `${product},${value}\n`);
   }
 }
-
-// Get the input file name from command line arguments
 const inputFileName = process.argv[2];
-
-if (!inputFileName) {
-  console.error('Please provide the input file name as a command line argument.');
-  process.exit(1);
-}
-
 processInput(inputFileName);
+
+module.exports = {
+  processInput,
+  countLines,
+  doesFileOrDirectoryExist,
+};
---
+++ unit_test.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const { processInput, countLines, writeOutput, doesFileOrDirectoryExist } = require('./app');
+const { expect } = require('chai');
+const { describe, it} = require('mocha');
+describe('countLines', () => {
+    it('should return the number of lines in a file', () => {
+        const testFileContent = 'Line 1\nLine 2\nLine 3';
+        fs.writeFileSync('testfile.csv', testFileContent);
+        const result = countLines('testfile.csv');
+        expect(result).to.equal(3);
+        fs.unlinkSync('testfile.csv');
+    });
+    it('should return 0 for an empty file', () => {
+        fs.writeFileSync('emptyfile.csv', '');
+        const result = countLines('emptyfile.csv');
+        expect(result).to.equal(0);
+        fs.unlinkSync('emptyfile.csv');
+    });
+    it('should return 0 for a non-existent file', () => {
+        const result = countLines('nonexistentfile.csv');
+        expect(result).to.equal(0);
+    });
+});
+describe('processInput', () => {
+    it('should handle a non-existent input file', () => {
+      const result = processInput('nonexistentfile.csv');
+      expect(result).to.equal(0);
+    });
+    it('should handle if there is less than 2 lines', () =>{
+        const testFileContent = 'Line 1';
+        fs.writeFileSync('testfile.csv', testFileContent);
+        const result = processInput('testfile.csv')
+        expect(result).to.equal('Minimum amount of lines is 2')
+        fs.unlinkSync('testfile.csv');
+    })
+    it('should handle if there is more or equals than 10^4 lines', () =>{
+        const testFileContent = Array.from({ length: 10001 }, (_, index) => `Line ${index + 1}`).join('\n');
+        fs.writeFileSync('testfile.csv', testFileContent);
+        const result = processInput('testfile.csv')
+        expect(result).to.equal('Maximum amount of lines is 10^4')
+        fs.unlinkSync('testfile.csv');
+    })
+  });
+
